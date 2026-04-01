dba711966874af42187975e85fa3c25b9abd57cc
TanKimzeg <1964375900@qq.com>
2026-04-01 18:32:18 +0800

    fix: 添加构建配置以支持多个入口文件

--- vite.config.ts
+++ vite.config.ts
@@ -27,4 +27,13 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        translator: "translator.html",
+      },
+    },
+  },
 }));
